4f0c57d7390374702976ba71e5823899ee52888a
sally <sallyhoney96@gmail.com>
2022-06-06 17:42:10 -0400

    add storelist component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,1 +1,10 @@
 import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p><a href="https://clearbit.com">Logos provided by Clearbit</a></p>
+    </div>
+  );
+}
